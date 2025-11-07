1b50b93a5eb7d339420708802c8bcd6e8fc38cd7
Chavely <chavely.study@gmail.com>
2025-11-07 18:33:31 -0500

    removing recovered files : adding gitignore lines

--- recover data/collect_src.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./collect_files.sh ./src/YuGiOh.Domain "cs csproj"
-./collect_files.sh ./src/YuGiOh.Application "cs csproj"
-./collect_files.sh ./src/YuGiOh.Infrastructure "cs csproj"
-./collect_files.sh ./src/YuGiOh.WebAPI "cs csproj"
-./collect_files.sh ./src/YuGiOh.UI/src "json tsx ts md js"
